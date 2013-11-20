fa92ad072e8ea55f7d5f79138aa76c72e0b85db1
Augusto1991 <oapetruccelli@gmail.com>
2013-11-20 01:42:39 -0300

    fixed BFOperator

--- Translator/assembler/BFOperator.java
+++ Translator/assembler/BFOperator.java
@@ -3,7 +3,7 @@ package assembler;
 public class BFOperator extends AbsSingleOperator {
 
 	public void generate(String operand) {  
-		CodeGenerator.assembler.add(CodeGenerator.check+" "+CodeGenerator.context+"_label"+ operand+":");
+		CodeGenerator.assembler.add(CodeGenerator.check+" "+CodeGenerator.context+"_label"+ operand);
 		//System.out.println(jumpType+" label_"+ direction + System.lineSeparator());
 	}
 }
