9fb0cc63c32271c4a1df3392539e8e3d592ed4dd
Inti <ipiolanti@gmail.com>
2013-11-21 04:24:04 -0300

    function calling update

--- Translator/assembler/FunctionCallOperator.java
+++ Translator/assembler/FunctionCallOperator.java
@@ -4,6 +4,7 @@ public class FunctionCallOperator extends AbsSingleOperator {
 
 	@Override
 	public void generate(String functionName) {
+		// en functionName+"_parameter" está el parámetro
 		CodeGenerator.assembler.add("CALL label_"+ functionName);
 	}
 
